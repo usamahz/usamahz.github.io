42c059709794dd717c1bd4bc552008e24778613b
U S A M A H <39458672+usamahz@users.noreply.github.com>
2025-03-22 23:15:30 +0000

    Fix bug in CI

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://usamahz.github.io',
+  base: '/usamahz.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
