58e256a57fa2ddaeacce6e6180da2cd9db483c72
U S A M A H <39458672+usamahz@users.noreply.github.com>
2025-03-22 23:13:17 +0000

    Create my blog

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Usamah - Blog';
+export const SITE_DESCRIPTION = 'My Personal Blog';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
