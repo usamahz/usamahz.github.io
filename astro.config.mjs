9f6313982529c23fc6c8379421163b01312daeb4
U S A M A H <39458672+usamahz@users.noreply.github.com>
2025-03-23 00:44:19 +0000

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://usamahz.github.io',
-  base: '/usamahz.github.io',
+  base: 'usamahz.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
